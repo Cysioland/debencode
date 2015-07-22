a8cf2275b2e39ab57a8f1cb8354f827708e03758
Joseph Durel <jadematrix.art@gmail.com>
2015-07-21 20:21:19 -0400

    Initial commit with basic defs

---
+++ debencode.cpp
@@ -0,0 +1,16 @@
+#include "debencode.hpp"
+
+
+
+#ifdef DEBENCODE_STANDALONE
+
+int main( int argc, char* argv[] )
+{
+    
+    
+    return 0;
+}
+
+#endif
+
+
---
+++ debencode.hpp
@@ -0,0 +1,8 @@
+#ifndef DEBENCODE_HPP
+#define DEBENCODE_HPP
+
+
+
+#endif
+
+
